16eceea4de5c7b95c7b947aa3b2338620c6c6434
Damian Melia <damian.melia@mail.utoronto.ca>
2023-07-22 21:56:34 -0400

    Setup Nav bar and the corresponding fragments (emtpy)

--- app/src/main/java/com/example/shoptrack/Home.java
+++ app/src/main/java/com/example/shoptrack/Home.java
@@ -1,14 +1,52 @@
 package com.example.shoptrack;
 
+import android.app.Activity;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.shoptrack.databinding.ActivityHomeBinding;
 
 
 public class Home extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+
+            if(item.getItemId() == R.id.nav_home){
+                replaceFragment(new HomeFragment());
+            }
+            else if (item.getItemId() == R.id.nav_cart){
+                replaceFragment(new CartFragment());
+            }
+            else if (item.getItemId() == R.id.nav_account){
+                replaceFragment(new AccountFragment());
+            }
+            else if (item.getItemId() == R.id.nav_settings){
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home_frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
 
 
 }
