469031b178c3194237bfc6d47ca4e428aaac6da5
BrandanBurgess <brandanbb13@gmail.com>
2023-07-24 12:34:52 -0400

    Removed title bar, fixed formatting of design.

--- app/src/main/java/com/example/shoptrack/MainActivity.java
+++ app/src/main/java/com/example/shoptrack/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // this line hides the action bar
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
