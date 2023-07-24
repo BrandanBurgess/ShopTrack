8263eefbd07ebca91b09af94c94e34717fdef473
Ken <kutlukanwar@gmail.com>
2023-07-24 12:20:38 -0400

    Creating products,stores

--- app/src/main/java/com/example/shoptrack/Store.java
+++ app/src/main/java/com/example/shoptrack/Store.java
@@ -1,4 +1,44 @@
 package com.example.shoptrack;
 
+import java.util.List;
+
 public class Store {
+    private String name;
+    private String description;
+    private String ownerId; //the id of the store owner
+    private List<Product> products;
+
+    public Store() {
+    }
+
+    public Store(String name, String description, String ownerId, List<Product> products) {
+        this.name = name;
+        this.description = description;
+        this.ownerId = ownerId;
+        this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        this.products.remove(product);
+    }
 }
--- app/src/main/java/com/example/shoptrack/StoreManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.shoptrack;
-
-public interface StoreManager {
-    void addStore(Store store);
-    void removeStore(Store store);
-    void updateStore(Store store);
-    void getStore(Store store);
-    void getAllStores();
-}
